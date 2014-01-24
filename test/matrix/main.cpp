da5b1e909e1ae35f9c805f28cb48e74377cd015e
superthierr14@gmail.com <cqy@cqy>
2014-01-24 15:01:07 +0800

    2014.1.24 chenqinyu

--- test/matrix/main.cpp
+++ test/matrix/main.cpp
@@ -2,8 +2,10 @@
 
 int main() {
 	for (UINT i = 0; i < 1; i++)	{
-		dense::RealMatrix *A = new dense::RealSquare(15);
-		dense::RealMatrix *B = new dense::RealSquare(15);
+		dense::RealMatrix *A = new dense::RealSquare(3);
+		dense::RealMatrix *B = new dense::RealSquare(3);
+		REAL M[3][3] = {0,1,2,1,2,3,2,3,4};
+		memmove((void *)A->M, (void *)M, 3 * 3 * sizeof(REAL));
 		dense::inv(B, A);
 		B->print();
 
